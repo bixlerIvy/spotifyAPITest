89ea07fd9b696222d12f0e7da30dc5a768720494
ibixler <108148378+ibixler@users.noreply.github.com>
2022-07-28 14:48:48 -0400

    added submodule and started spotify api work

---
+++ js/nodeTest.js
@@ -0,0 +1,9 @@
+console.log("test")
+
+var http = require("http");
+
+http.createServer(function(request, response) {
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.write("hello");
+    response.end();
+}).listen(8888);
